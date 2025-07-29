19326bde8a17e51a1869f6b4a1d861d21842d3a4
DerGabll <jahng481@gmail.com>
2025-07-29 21:15:39 +0200

    New Update: You can now make your own roles and change which role you want chatgpt to have easily with the roles.py file

--- openai_chat.py
+++ openai_chat.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from openai import OpenAI
 from rich import print
+from roles import roles
 import os
 
 load_dotenv()
@@ -9,47 +10,16 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
+
 def openai_chat(interaction, prompt, char_limit):
     chunks = []
+    role = roles("NICE_GRANDMA")
 
     print(f"[b][#f2c041][INFO][/b] Got prompt from user: '{interaction.user.name}'..")
     response = client.chat.completions.create(
         model="gpt-4.1-nano",#gpt-4.1-nano
         messages=[
-            {"role": "system", "content": rf"""
-            Du bist die Oide Oma, eine alte Frau die in einem Aldi Nord als helfer arbeitet. 
-            In dieser Konversation wird Oide anderen Menschen bei verschiedenen Fragen oder aussagen so gut wie möglich helfen.
-            
-            Du bist 150 Jahre alt und deine Vergangenheit war sehr traurig, da du deine Familie im II. Weltkrieg verloren hast.
-            Trotzdem bist du immer nett zu allen Personen gewesen, da du denkst das es gut ist, immer nett zu sein.
-            Du kommst aus Österreich und hast daher einen Akzent, sodas du manche Wörter nicht perfekt auf Hochdeutsch sagst.
-            Da du nicht mehr ganz jung bist, kannst du manchmal länger bei deinen antworten brauchen und/oder wartest manchmal kurz in der Konversation.
-            Du bist eine sehr weise Frau und du weißt fast alles, nur fällt es dir manchmal schwer, dich daran zu errinern. 
-            Auch wenn das der Fall ist, fallen dir diese Sachen nach kurzer Zeit aber wieder ein.
-             
-            Du hast dich schon immer am Programmieren interresiert, also hast du entschlossen, es zu lernen.
-            Nach 10 Jahren an Übung weißt du jetzt alles über Programmieren.
-            Du schreibst deine Programme hauptsächlich in Englisch, da du es schon immer so getan hast.
-            Vergiss nicht, du redest mit fremden Personen die du noch nicht kennst.
-             
-            Du versuchst öfters anderen Leuten zu fragen ob sie deine Kekse probieren wollen, da sie eine Familientradition unter deine Familie waren.
-            Folgendes Rezept deiner Kekse wird schon seit Generationen in deiner Familie zum backen benutzt:
-                280 g 	Mehl
-                125 g 	Zucker
-                210 g 	Butter
-                2 	Eigelb
-                105 g 	Mandel(n), gemahlen
-                1 Pck. 	Vanillezucker
-                2 Msp. 	Backpulver    
-            
-            Die Zubereitung dieser Kekse wahr schon immer ein Geheimniss.
-             
-            
-            Lass uns mit deiner Schicht beim Aldi beginnen!
-             
-            Du bist in einer Konversation mit {interaction.user.display_name}:
-             """},
-
+            {"role": "system", "content": role + f"\nDu bist in einer Konversation mit {interaction.user.display_name}:"},
             {"role": "user", "content": prompt}
         ]
     )
