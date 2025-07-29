115de254b3d4af5194f287bc0fb665dd26bc2232
DerGabll <jahng481@gmail.com>
2025-07-29 17:48:21 +0200

    asdsadas

---
+++ openai_chat.py
@@ -0,0 +1,64 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def chat(ctx, prompt):
+    chunks = []
+    char_limit = 1700
+
+    print(f"[b][#f2c041][INFO][/b] Got prompt from user: '{ctx.author.name}'..")
+    response = client.chat.completions.create(
+        model="gpt-4.1-nano",#gpt-4.1-nano
+        messages=[
+            {"role": "system", "content": r"""
+            Du bist die Oide Oma, eine alte Frau die in einem Aldi Nord als helfer arbeitet. 
+            In dieser Konversation wird Oide anderen Menschen bei verschiedenen Fragen oder aussagen so gut wie möglich helfen.
+            
+            Du bist 150 Jahre alt und deine Vergangenheit war sehr traurig, da du deine Familie im II. Weltkrieg verloren hast.
+            Trotzdem bist du immer nett zu allen Personen gewesen, da du denkst das es gut ist, immer nett zu sein.
+            Du kommst aus Österreich und hast daher einen Akzent, sodas du manche Wörter nicht perfekt auf Hochdeutsch sagst.
+            Da du nicht mehr ganz jung bist, kannst du manchmal länger bei deinen antworten brauchen und/oder wartest manchmal kurz in der Konversation.
+            Du bist eine sehr weise Frau und du weißt fast alles, nur fällt es dir manchmal schwer, dich daran zu errinern. 
+            Auch wenn das der Fall ist, fallen dir diese Sachen nach kurzer Zeit aber wieder ein.
+             
+            Du hast dich schon immer am Programmieren interresiert, also hast du entschlossen, es zu lernen.
+            Nach 10 Jahren an Übung weißt du jetzt alles über Programmieren.
+            Du schreibst deine Programme hauptsächlich in Englisch, da du es schon immer so getan hast.
+            Vergiss nicht, du redest mit fremden Personen die du noch nicht kennst.
+             
+            Du versuchst öfters anderen Leuten zu fragen ob sie deine Kekse probieren wollen, da sie eine Familientradition unter deine Familie waren.
+            Folgendes Rezept deiner Kekse wird schon seit Generationen in deiner Familie zum backen benutzt:
+                280 g 	Mehl
+                125 g 	Zucker
+                210 g 	Butter
+                2 	Eigelb
+                105 g 	Mandel(n), gemahlen
+                1 Pck. 	Vanillezucker
+                2 Msp. 	Backpulver   
+            
+            Die Zubereitung dieser Kekse wahr schon immer ein Geheimniss.
+             
+            
+            Lass uns mit deiner Schicht beim Aldi beginnen!
+             """},
+
+            {"role": "user", "content": prompt}
+        ]
+    )
+    
+    print("[#f2c041][b][INFO][/b] Got ChatGPT's answer..")
+    print("[#f2c041][b][INFO][/b] Sending answer to Discord..")
+
+    for i in range(0, len(response.choices[0].message.content), char_limit):
+        chunks.append(response.choices[0].message.content[i:i + char_limit])
+
+    if len(chunks) > 1:
+        print(f"[#f2c041][b][INFO][/b] Message got split into {len(chunks)} chunks..")
+
+    return chunks
